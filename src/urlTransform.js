6324cfcd52eb79c9eab33f5dc59c1fcc1a12db69
Matt Apperson <matt@appersonlabs.com>
2015-08-22 10:34:39 -0400

    Support for more complex URLs

--- src/urlTransform.js
+++ src/urlTransform.js
@@ -1,7 +1,26 @@
 "use strict";
 import reduce from "lodash/collection/reduce";
+import qs from "qs";
 
 export default function urlTransform(url, params={}) {
-  return reduce(params,
-    (url, value, key)=> url.replace(new RegExp(`:${key}`, "g"), value), url);
+  var urlWithParams = reduce(params,
+    (url, value, key) => {
+      url = url.replace(new RegExp(`\\(:${key}\\)`, "g"), function() {
+        delete params[key];
+        return value;
+      });
+      url = url.replace(new RegExp(`:${key}`, "g"), function() {
+        delete params[key];
+        return value;
+      });
+      return url;
+    }, url);
+  if (!urlWithParams) return urlWithParams;
+  var cleanURL = urlWithParams.replace(new RegExp(`\\(:(.*?)\\)`, "g"), "");
+
+  var finalURL = cleanURL;
+  if (Object.keys(params).length > 0) {
+    finalURL = `${cleanURL}?${qs.stringify(params)}`;
+  }
+  return finalURL;
 }
--- test/urlTransform_spec.js
+++ test/urlTransform_spec.js
@@ -13,6 +13,13 @@ describe("urlTransform", function() {
   it("check replace path", function() {
     expect(urlTransform("/test/:id", {id: 1})).to.eql("/test/1");
     expect(urlTransform("/test/:id/hey/:id", {id: 1})).to.eql("/test/1/hey/1");
-    expect(urlTransform("/test/:id", {id1: 1})).to.eql("/test/:id");
+  });
+  it("check optional params path", function() {
+    expect(urlTransform("/test/:id", {id: 1})).to.eql("/test/1");
+    expect(urlTransform("/test/(:id)", {id: 1})).to.eql("/test/1");
+    expect(urlTransform("/test/(:id)")).to.eql("/test/");
+  });
+  it("chech non-pretty params in path", function() {
+    expect(urlTransform("/test/(:id)", {id1: 1})).to.eql("/test/?id1=1");
   });
 });
