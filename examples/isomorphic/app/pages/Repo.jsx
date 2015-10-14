a1e083fe0d565dc67da9d1679de0855261e6be7f
Efremov Alexey <lexich121@gmail.com>
2015-10-14 09:00:19 +0300

    Update: example npm + fix example

--- examples/isomorphic/app/pages/Repo.jsx
+++ examples/isomorphic/app/pages/Repo.jsx
@@ -5,17 +5,18 @@ import { Link } from "react-router";
 class Repo extends React.Component {
   render() {
     const { repo } = this.props;
-    const data = repo.data;
+    const data = repo.data || {};
+    const owner = data.owner || {};
     return !repo.loading ? (
       <div className="Repo media">
         <div className="media-left">
-          <Link to={`/${data.owner.login}`}>
-            <img className="Repo__media media-object" src={data.owner.avatar_url } />
+          <Link to={`/${owner.login}`}>
+            <img className="Repo__media media-object" src={owner.avatar_url } />
           </Link>
         </div>
         <div className="media-body">
           <h4>{ data.name }</h4>
-          <Link to={`/${data.owner.login}`}>@{ data.owner.login }</Link>&nbsp;
+          <Link to={`/${owner.login}`}>@{ owner.login }</Link>&nbsp;
           Stars { data.stargazers_count } Forks {data.forks }
         </div>
       </div>) : ( <div className="Repo">Loading</div> );
