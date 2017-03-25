0b313b1657d16bf275cd63df79e192ed1cf64d01
Efremov Alexey <lexich121@gmail.com>
2017-03-25 19:26:46 +0300

    Refactoring

---
+++ test/cache_spec.js
@@ -0,0 +1,29 @@
+/* global describe, it */
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}], no-void: 0 */
+
+import { expect } from "chai";
+import cache, { Manager } from "../src/utils/cache";
+
+describe("cache-manager", ()=> {
+  it("check empty call", ()=> {
+    expect(cache()).to.not.exist;
+    expect(cache(null)).to.not.exist;
+    expect(cache(false)).to.not.exist;
+  });
+
+  it("check cache as true", ()=> {
+    const manager = cache(true);
+    expect(manager === Manager).to.be.true;
+    expect(manager.id).to.exist;
+    expect(manager.id({ a: 1, b: 2 })).to.eql("a=1;b=2;");
+  });
+
+  it("check cache rewrite id", ()=> {
+    const manager = cache({
+      id(params, opts) {
+        return Manager.id(params) + opts;
+      }
+    });
+    expect(manager.id({ a: 1, b: 2 }, "extra")).to.eql("a=1;b=2;extra");
+  });
+});
